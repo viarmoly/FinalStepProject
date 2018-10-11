a7877ed0fe1921c87d2d1efc9e119d04edf3ecb0
flatdeh <flatdeh@gmail.com>
2018-10-11 19:01:22 +0300

    Create spring boot app (#6)
    * create-spring-boot-app
    
    * create 'backend' folder

--- backend/src/main/java/com/danIt/enerGym/EnerGymApplication.java
+++ backend/src/main/java/com/danIt/enerGym/EnerGymApplication.java
@@ -1,12 +1,12 @@
-package com.energym.energym;
+package com.danIt.enerGym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnergymApplication {
+public class EnerGymApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EnergymApplication.class, args);
+		SpringApplication.run(EnerGymApplication.class, args);
 	}
 }
--- backend/src/test/java/com/danIt/enerGym/EnerGymApplicationTests.java
+++ backend/src/test/java/com/danIt/enerGym/EnerGymApplicationTests.java
@@ -1,4 +1,4 @@
-package com.energym.energym;
+package com.danIt.enerGym;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EnergymApplicationTests {
+public class EnerGymApplicationTests {
 
 	@Test
 	public void contextLoads() {
