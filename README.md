a3c7c428bab3a99939a01471b722c4b2ab10ecc7
viktorterljuk <38193309+viktorterljuk@users.noreply.github.com>
2018-10-12 16:57:45 +0300

    Rebuild project (#22)
    * created empty react app
    
    * changet structure of project, added based security
    
    * removed target-folder from git

---
+++ README.md
@@ -0,0 +1,3 @@
+# LuckyFit App
+
+![Logo](misc/logo.png?raw=true "App Logo")
---
+++ src/main/java/org/danit/LuckFitApplication.java
@@ -0,0 +1,12 @@
+package org.danit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckFitApplication.class, args);
+    }
+}
