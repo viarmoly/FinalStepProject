9ea5776566c73e544586188a32817f8be5807f25
viktorterljuk <38193309+viktorterljuk@users.noreply.github.com>
2018-11-05 18:54:40 +0200

    Cleaned frontend (#54)
    * created empty react app
    
    * cleaned frontend directory

--- frontend/src/components/LoginPage/index.js
+++ frontend/src/components/LoginPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import {userActions} from '../../actions';
+import {userActions} from '../../actions/index';
 
 class LoginPage extends React.Component {
     constructor(props) {
