2d1f2293612a9254586d3995f4f7b2cf91b237b9
flatdeh <flatdeh@gmail.com>
2018-11-09 00:09:54 +0200

    Create react component for adding statuses, implement deserializer for status (#63)
    * Create react component for adding statuses, implement deserializer for statusЭ
    
    * Refactor

--- src/main/java/org/danit/luckyfit/entity/Status.java
+++ src/main/java/org/danit/luckyfit/entity/Status.java
@@ -1,9 +1,15 @@
 package org.danit.luckyfit.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import org.danit.luckyfit.StatusDeserializer;
 
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -12,19 +18,22 @@ import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
 
+@JsonDeserialize(using = StatusDeserializer.class)
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "status")
 public class Status {
-
   @Id
-  @GeneratedValue
-  @Column(name = "status_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "status_id", unique = true, nullable = false)
   private long id;
 
-  @NotNull
+  @NonNull
   @Column(name = "status_name")
   private String name;
 
